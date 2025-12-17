f90020ed906c53eb333e1ab483109b97f42cc5ed
Manirohith08 <135613002+Manirohith08@users.noreply.github.com>
2025-12-17 17:09:37 +0530

    Update AIModal.jsx

--- src/service/AIModal.jsx
+++ src/service/AIModal.jsx
@@ -5,7 +5,7 @@ const genAI = new GoogleGenerativeAI(apiKey);
 
 // 👇 This selects the Gemini 2.0 Flash model
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash",
+  model: "gemini-2.5-flash",
 });
 
 const generationConfig = {
